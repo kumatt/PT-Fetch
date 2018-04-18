b5ef7b3cb786edf00b679089e32292b829949e64
ZhouKangYi <blank-winter@foxmail.com>
2018-04-18 16:51:58 +0800

    [Edit] try catch

--- PT-FetchDemo/PT-FetchDemo/PublicFetchTarget.swift
+++ PT-FetchDemo/PT-FetchDemo/PublicFetchTarget.swift
@@ -9,21 +9,70 @@
 import UIKit
 
 class PublicFetchTarget: PTFetchProtocol {
+    
+    /// 对数据的处理结果抛错
+    enum PublicFetchError:Error {
+        /// 数据解析错误
+        case NullData
+        
+        /// 后台提示错误信息
+        case ErrorMessage
+        
+        /// 未知错误
+        case UnknownError
+    }
+    
+    func dealDataInZhangGui(data:Data) throws -> Dictionary<String,Any> {
+        if data.isEmpty {
+            throw  PublicFetchError.NullData
+        }
+        let dict:Dictionary<String,Any> = (try!JSONSerialization.jsonObject(with: data, options: .allowFragments) as? Dictionary<String,Any>)!
+        if dict.isEmpty {
+            throw  PublicFetchError.NullData
+        }
+        if dict["code"] as? Int == 0 {
+            return dict
+        }
+        if ((dict["message"] as? String) != nil) {
+            throw PublicFetchError.ErrorMessage
+        }
+        throw PublicFetchError.UnknownError
+    }
+    
+    func dealRulesWith(data: Data, success: (Any) -> Void, failure: (Any) -> Void) {
+        do{
+            let dict:Dictionary<String,Any> = try dealDataInZhangGui(data: data)
+            success(dict)
+        }
+        catch{
+            switch error {
+            case PublicFetchError.NullData:
+                failure("空数据")
+            case PublicFetchError.UnknownError:
+                failure("数据异常")
+            case PublicFetchError.ErrorMessage:
+                do {
+                    failure((try!JSONSerialization.jsonObject(with: data, options: .allowFragments) as? Dictionary<String,Any>)!["message"]! as Any)
+                }
+            default:
+                failure("网络异常")
+            }
+        }
+    }
+    
     func appentUrlWithShortUrl(url: String!) -> String? {
-        return url
+        return String(url)
     }
     
     func appentParamentsWithShortParament(parament: Dictionary<String, Any>!) -> Dictionary<String, Any>? {
         return parament
     }
     
-    func dealRulesWith(data: Data, success: (Any) -> Void, failure: (Any) -> Void) {
-        
-    }
-    
     func dealErrorData(error: Any) -> Any {
-        return error
+        if error is String {
+            return error
+        }
+        return "网络异常"
     }
-    
-    
+
 }
